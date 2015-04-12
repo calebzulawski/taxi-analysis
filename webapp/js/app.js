ace466fa20ed1fc92cdc85a1424a8bc11d959132
Caleb Zulawski <caleb.zulawski@gmail.com>
2015-04-12 01:27:13 -0400

    Updated web interface

--- webapp/js/app.js
+++ webapp/js/app.js
@@ -11,4 +11,30 @@ angular.module('mapApp').config(function(uiGmapGoogleMapApiProvider) {
 
 angular.module('mapApp').controller('mainCtrl', ['$scope', function($scope) {
 	$scope.map = { center: { latitude: 40.7127, longitude: -74.0059 }, zoom: 11 };
+    $scope.options = {
+        minZoom: 11,
+        streetViewControl: false,
+        zoomControl: false,
+        panControl: false
+    }
+}]);
+
+
+angular.module('mapApp').controller('sideCtrl', ['$scope','$http', function($scope, $http) {
+    $http.get('json/list.json').success(function(data) { 
+        $scope.list = data
+    });
+
+    /*
+    $scope.list = {
+        overlays: [
+            { url: 'json/1.json', title: 'Some data thing'},
+            { url: 'json/2.json', title: 'Some other thing'}
+        ]
+    }
+    */  
+
+    $scope.loadJson = function(url) {
+        alert(url)
+    }
 }]);
