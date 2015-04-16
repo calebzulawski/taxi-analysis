ee657494180264d8e44988087606b5a531237008
Caleb Zulawski <zulaws@senior02.ee.cooper.edu>
2015-04-15 22:35:22 -0400

    Changed source of boundaries

--- tools/neighborhoodinfo.py
+++ tools/neighborhoodinfo.py
@@ -7,19 +7,17 @@ import csv
 def descend(jsonobj,depth=0,maxdepth=3):
 	result = {}
 	parent = jsonobj['path']
-
-	if (len(jsonobj['descendents']) == 0) and ('boundary_encoded_points_string' in jsonobj):
+	if ('boundary_encoded_points_string' in jsonobj):
+		print(depth)
 		result[jsonobj['path']] = {}
 		result[jsonobj['path']]['path'] = parent
 		result[jsonobj['path']]['title'] = jsonobj['title']
 		result[jsonobj['path']]['subtitle'] = jsonobj['subtitle']
 		result[jsonobj['path']]['name'] = jsonobj['name']
-		result[jsonobj['path']]['city'] = jsonobj['city']
-		result[jsonobj['path']]['state'] = jsonobj['state']
 		result[jsonobj['path']]['boundary'] = jsonobj['boundary_encoded_points_string']
-	if depth < maxdepth:
+	elif depth < maxdepth:
 		for descendent in jsonobj['descendents']:
-			deeper = descend(descendent,depth=depth+1,maxdepth=maxdepth)
+			deeper = descend(descendent,depth=(depth+1),maxdepth=maxdepth)
 			result.update(deeper)
 	return result
 
@@ -30,13 +28,13 @@ url = "http://streeteasy.com/nyc/api/areas/deep_info?id=1&key=" + apikey + "&for
 response = urllib.request.urlopen(url)
 response_str = response.readall().decode('utf-8')
 data = json.loads(response_str)
-neighborhoods = descend(data)
+neighborhoods = descend(data,maxdepth=4)
 
 for key in neighborhoods:
 	neighborhoods[key]['id'] = list(neighborhoods.keys()).index(key)
 
-with open('neighborhoods-boundarys.csv','w') as csvfile:
+with open('boundaries.csv','w') as csvfile:
 	csvwriter = csv.writer(csvfile)
-	csvwriter.writerow(['id', 'path', 'title', 'subtitle', 'name', 'city', 'state'])
+	csvwriter.writerow(['id', 'path', 'title', 'subtitle', 'name'])
 	for key in neighborhoods:
-		csvwriter.writerow([ neighborhoods[key]['id'], neighborhoods[key]['path'], neighborhoods[key]['boundary'], neighborhoods[key]['title'], neighborhoods[key]['subtitle'], neighborhoods[key]['name'], neighborhoods[key]['city'], neighborhoods[key]['state'] ])
+		csvwriter.writerow([ neighborhoods[key]['id'], neighborhoods[key]['path'], neighborhoods[key]['boundary'], neighborhoods[key]['title'], neighborhoods[key]['subtitle'], neighborhoods[key]['name']])
--- webapp/js/app.js
+++ webapp/js/app.js
@@ -10,6 +10,7 @@ angular.module('mapApp').config(function(uiGmapGoogleMapApiProvider) {
 })
 
 angular.module('mapApp').controller('mainCtrl', ['$scope','$http', function($scope, $http) {
+    //Set starting parameters for the map
     $scope.map = {
         center: { latitude: 40.7127, longitude: -74.0059 },
         zoom: 11,
@@ -23,8 +24,54 @@ angular.module('mapApp').controller('mainCtrl', ['$scope','$http', function($sco
     };
     $scope.control = {}
 
+    //Load polygons
     var jsonRoot = 'webapp/json/'
     $scope.polygons = []
+
+    $http.get(jsonRoot + 'community_districts.geojson').success(function(data) { 
+        var id = 0;
+        for (var i = 0; i < data.features.length; i++) {
+            var item = data.features[i]
+            if (item.geometry.type === 'Polygon') {
+                var temppath = [];
+                for (var j = 0; j < item.geometry.coordinates[0].length; j++){
+                    var point = item.geometry.coordinates[0][j];
+                    temppath.push( {latitude: point[1], longitude: point[0]} )
+                }
+
+                $scope.polygons.push( {
+                    id: id,
+                    path: temppath,
+                    fill: {color: '#000000', opacity: 0.1},
+                    stroke: {color: '#000000', weight: 2, opacity: 1},
+                    cityid: item.properties.BoroCD
+                    } )
+
+                id++;
+            }
+            if (item.geometry.type === 'MultiPolygon') {
+                for (var j = 0; j < item.geometry.coordinates.length; j++){
+                    var poly = item.geometry.coordinates[j];
+                    var temppath = [];
+                    for (var k = 0; k < poly[0].length; k++){
+                        point = poly[0][k];
+                        temppath.push( {latitude: point[1], longitude: point[0]} )
+                    }
+
+                    $scope.polygons.push( {
+                        id: id,
+                        path: temppath,
+                        fill: {color: '#000000', opacity: 0.1},
+                        stroke: {color: '#000000', weight: 2, opacity: 1},
+                        cityid: item.properties.BoroCD
+                        } )
+
+                    id++;
+                }
+            }
+        }
+    }); 
+
 
     $http.get(jsonRoot + 'list.json').success(function(data) { 
         $scope.list = data
@@ -32,8 +79,8 @@ angular.module('mapApp').controller('mainCtrl', ['$scope','$http', function($sco
 
     $scope.loadJson = function(url) {
         $http.get(jsonRoot + url).success(function(data) {
-              $scope.polygons = data
-              $scope.currentTitle = data.title
+              //$scope.polygons = data
+              //$scope.currentTitle = data.title
         });
         //$scope.control.refresh({ latitude: 40.7127, longitude: -74.0059 })
     }
